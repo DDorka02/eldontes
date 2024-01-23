758c359962b1ee5d3c1db873611736865a81e803
DDorka02 <dobszaydorka@gmail.com>
2024-01-23 10:26:02 +0100

    eldontes2

--- Szamitogep.py
+++ Szamitogep.py
@@ -1,4 +1,4 @@
-class Szamtogep:
+class Szamitogep:
     def __init__(self, oprsz, ram):
         self.oprsz = oprsz
         self.ram = ram
--- szamitogep_program.py
+++ szamitogep_program.py
@@ -1,11 +1,13 @@
-import Szamitogep
-from Szamitogep import Szamtogep
+from Szamitogep import Szamitogep
 
 peldany1 = Szamitogep("win", 32)
 peldany2 = Szamitogep("mac", 16)
 peldany3 = Szamitogep("win", 16)
 
+
 szamitogepek = []
+
+
 szamitogepek.append(peldany1)
 szamitogepek.append(peldany2)
 szamitogepek.append(peldany3)
@@ -27,3 +29,23 @@ for i in range(len(szamitogepek)):
     if szamitogepek[i].ram > szamitogepek[index].ram:
         index = i
 print(f"{szamitogepek[index].oprsz}")
+
+# megszamlálás
+db = 0
+print("Hány windows-os gép van? ", end="")
+for i in range(len(szamitogepek)):
+    if szamitogepek[i].oprsz == "win":
+        db += 1
+print(f"{db} db.")
+
+vizsgaltram = 48
+print(f"Van-e {vizsgaltram} Gb-nál nagyobb windows? ", end="")
+dontes = False
+for i in range(len(szamitogepek)):
+    if szamitogepek[i].ram > vizsgaltram and szamitogepek[i].oprsz == "win":
+        dontes = True
+
+if dontes == True:
+    print("Van ilyen gép")
+else:
+    print("Nincs ilyen gép")
